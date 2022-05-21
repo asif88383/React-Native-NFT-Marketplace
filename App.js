5a2c6024eab67e61adf0ddcdfb3864cf68f6e917
Muhammad Asif <asif88383@gmail.com>
2022-05-21 22:56:21 +0500

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import Home from "./screens/Home";
+import Details from "./screens/Detail";
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator>
-        <Stack.Screen/>
-        <Stack.Screen/>
-      </StackNavigator>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Details" component={Details}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
