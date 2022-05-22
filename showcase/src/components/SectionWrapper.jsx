cfc19b855547cd5ce4d17207f66a737fe991b3f0
Muhammad Asif <asif88383@gmail.com>
2022-05-22 17:01:23 +0500

    first commit

--- showcase/src/components/SectionWrapper.jsx
+++ showcase/src/components/SectionWrapper.jsx
@@ -3,25 +3,52 @@ import styles from "../styles/Global";
 import assets from "../assets";
 import Button from "./Button";
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
   return (
     <di
-      className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
+      className={`min-h-screen ${styles.section} 
+      ${reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}
     >
-      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
-        <div className={`${styles.descDiv}`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <h1 className={`${styles.descriptionText}`}>{description}</h1>
+      <div
+        className={`flex items-center 
+      ${
+        reverse ? styles.boxReverseClass : styles.boxClass
+      } w-11/12 sm:w-full minmd:w-3/4`}
+      >
+        <div
+          className={`${styles.descDiv}
+        ${reverse ? "fadeRightMini" : "fadeLeftMini"}
+        ${reverse ? styles.textRight : styles.textLeft}
+        `}
+        >
+          <h1
+            className={`
+          ${reverse ? styles.blackText : styles.whiteText}
+          ${styles.h1Text}`}
+          >
+            {title}
+          </h1>
+          <h1
+            className={`
+          ${reverse ? styles.blackText : styles.whiteText}
+          ${styles.descriptionText}`}
+          >
+            {description}
+          </h1>
           {showBtn && (
-             <Button 
-              assetUrl={assets.expo}
-              link="Deployed NFT marketplace RN"
-             />
+            <Button assetUrl={assets.expo} link="Deployed NFT marketplace RN" />
           )}
         </div>
 
         <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
-           <img src={mockupImg} alt="mockup" />
+          <img
+            src={mockupImg}
+            alt="mockup"
+            className={`
+          ${reverse ? "fadeLeftMini" : "fadeRightMini"}
+          ${styles.sectionImg}
+          `}
+          />
         </div>
       </div>
     </di>
